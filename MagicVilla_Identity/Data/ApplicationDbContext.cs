f9e5ce01d92f82e12467e05c25ec154e9012f9c6
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-07-04 14:25:43 +0800

    lesson 14  applicationdbcontext and off licinse

---
+++ MagicVilla_Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MagicVilla_Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MagicVilla_Identity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
